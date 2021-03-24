d883e1d36563dcc0092cde74628f6d4cfd0f27b0
arorakashish <50692447+arorakashish@users.noreply.github.com>
2021-03-24 17:26:48 +0530

    Get username from auth service

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, OnInit, Input } from "@angular/core";
+import { FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-input',
-  templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+  selector: "app-input",
+  templateUrl: "./input.component.html",
+  styleUrls: ["./input.component.css"],
 })
 export class InputComponent implements OnInit {
   @Input() label: string;
   @Input() control: FormControl;
   @Input() inputType: string;
+  @Input() controlType = "input";
 
   constructor() {}
 
